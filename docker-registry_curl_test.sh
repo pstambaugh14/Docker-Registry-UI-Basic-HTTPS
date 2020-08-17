8d3fcd321aa80018ac7c49bb85b200aad29ffb31
Patrick Stambaugh <pstambaugh14@gmail.com>
2020-08-17 04:37:15 -0400

    More Stuff to Add

--- docker-registry_curl_test.sh
+++ docker-registry_curl_test.sh
@@ -4,9 +4,9 @@ source ./host
 
 PWD=`pwd`
 
-CERT=`find "${PWD}/certs/registry_ca_certs" -type f -name 'cert.pem'`
-KEY=`find "${PWD}/certs/registry_ca_certs" -type f -name 'key.pem'`
-CA=`find "${PWD}/certs/registry_ca_certs" -type f -name 'ca.pem'`
+#CERT=`find "${PWD}/certs/registry_ca_certs" -type f -name 'cert.pem'`
+#KEY=`find "${PWD}/certs/registry_ca_certs" -type f -name 'key.pem'`
+#CA=`find "${PWD}/certs/registry_ca_certs" -type f -name 'ca.pem'`
 
 #curl https://"${HOST}":2376/images/json   --cert "${CERT}" --key "${KEY}"   --cacert "${CA}"
 
