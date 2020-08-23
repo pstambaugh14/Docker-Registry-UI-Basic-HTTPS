e97134e2badae792faab5e7e10a4c83da44729fd
Patrick Stambaugh <pstambaugh14@gmail.com>
2020-08-23 08:48:20 -0400

    Slight Mods

--- docker-daemon_curl_test.sh
+++ docker-daemon_curl_test.sh
@@ -5,4 +5,4 @@ source ./host
 curl https://$HOST:2376/images/json \
   --cert ~/.docker/certs/cert.pem \
   --key ~/.docker/certs/key.pem \
-  --cacert ~/.docker/certs/ca.pem
+  --cacert ~/.docker/certs/ca.pem | jq
